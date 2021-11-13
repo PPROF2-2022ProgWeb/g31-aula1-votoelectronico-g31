be0d56e272838275face849d946c0565e253bab8
FlorenciaBravoConsultoresIT <florcorvalan26@gmail.com>
2021-11-13 20:12:35 -0300

    actualiza

--- VOTO_ELECTRONICO/js/funcionesGrupo06.js
+++ VOTO_ELECTRONICO/js/funcionesGrupo06.js
@@ -58,10 +58,3 @@ function existeFecha(){
   return true;
 }
 
-function existeFecha2 () {
-    var fechaf = fecha.split("/");
-    var d = fechaf[0];
-    var m = fechaf[1];
-    var y = fechaf[2];
-    return m > 0 && m < 13 && y > 0 && y < 32768 && d > 0 && d <= (new Date(y, m, 0)).getDate();
-}
