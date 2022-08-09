4e23d77a669dcfb1610a690ce319c93c2d3a122d
FlorenciaBravoConsultoresIT <florcorvalan26@gmail.com>
2022-08-08 21:12:27 -0300

    formularios agregados

--- ANGULAR/src/app/app.module.ts
+++ ANGULAR/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,8 @@ import { PagesModule } from './pages/pages.module';
     BrowserModule,
     LayoutModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
