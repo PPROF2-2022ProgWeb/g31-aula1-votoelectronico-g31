939b2d89ca8f5671826b1961fe09da0eaf7fac43
Juan Ortega <91577323+Full-Juan-Ortega@users.noreply.github.com>
2022-09-18 21:07:33 -0300

    rama en blanco

--- Back-End/votoBack/votoBack/src/main/java/com/votoElectronico/votoBack/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.votoElectronico.votoBack.repository;
-
-import com.votoElectronico.votoBack.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
