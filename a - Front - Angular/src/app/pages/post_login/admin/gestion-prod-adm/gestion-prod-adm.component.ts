288366855cbe153b237264048da2debd80f76d4b
Lucas Bartolone <lucasbarto_01@outlook.com>
2022-11-10 18:05:05 -0300

    Finalizado frontend de user/admin

--- a - Front - Angular/src/app/pages/post_login/admin/gestion-prod-adm/gestion-prod-adm.component.ts
+++ a - Front - Angular/src/app/pages/post_login/admin/gestion-prod-adm/gestion-prod-adm.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-gestion-prod-adm',
@@ -10,6 +12,45 @@ export class GestionProdAdmComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<Producto>(this.datos);
+    this.dataSource.paginator = this.paginator;
   }
 
+  filtrar(event: Event) {
+    const filtro = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filtro.trim().toLowerCase();
+  }
+
+  borrarFila(cod: number) {
+    if (confirm("¿Realmente quiere borrar los datos?")) {
+      this.datos.splice(cod, 1);
+      this.tabla3.renderRows();
+    }
+  }
+
+  columnas: string[] = ['productoid','nombre','descripcion','precio','imagen','categoria','borrar','editar'];
+
+  datos: Producto[] =
+      [new Producto(1,'Votacion gratuita','Hasta 10 votantes',0,'','gratuito',),
+      new Producto(2,'Votacion estandar','Hasta 50 votantes',1000,' ','pago',),
+      new Producto(3,'Votacion premium','Votantes ilimitados',2000,' ','pago',),]
+    dataSource: any;
+
+    Productoselect: Producto = new Producto(1,'a','b',123,'c','d',);
+  
+    @ViewChild(MatTable) tabla3!: MatTable<Producto>;
+    @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+}
+
+export class Producto {
+  constructor(
+    public productoid: number,
+    public nombre: string,
+    public descripcion: string,
+    public precio: number,
+    public imagen: any,
+    public categoria: string,
+    ) {
+  }
 }
