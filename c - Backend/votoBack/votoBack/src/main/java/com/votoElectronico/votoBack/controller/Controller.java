dea9b721326a4dbec913fd40ec96b8212df1f8d7
Hilario Iñigo Menendez Picossi <32109140+hpicossi@users.noreply.github.com>
2022-11-12 18:49:22 -0300

    Agregando métodos de carrito

--- c - Backend/votoBack/votoBack/src/main/java/com/votoElectronico/votoBack/controller/Controller.java
+++ c - Backend/votoBack/votoBack/src/main/java/com/votoElectronico/votoBack/controller/Controller.java
@@ -2,12 +2,17 @@ package com.votoElectronico.votoBack.controller;
 
 import com.votoElectronico.votoBack.model.Usuario;
 import com.votoElectronico.votoBack.repository.UsuarioRepository;
+import com.votoElectronico.votoBack.service.CartItemService;
 import com.votoElectronico.votoBack.service.IUsuarioService;
+import com.votoElectronico.votoBack.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins="http://localhost:4200")
 @RestController
@@ -18,6 +23,18 @@ public class Controller {
     private UsuarioRepository usuarioRepository;
 
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    //----------
+//    private final UserService userService;
+//    private final ProductService productService;
+//    private final CartItemService cartItemService;
+//
+//    @Autowired
+//    private JwtUserDetailsService jwtUserDetailsService;
+//
+//    @Autowired
+//    private JwtUtil jwtUtil;
+    //---------
 
     public Controller(UsuarioRepository usuarioRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.usuarioRepository = usuarioRepository;
@@ -59,7 +76,71 @@ public class Controller {
     @PutMapping("/editarUser")
     public void editarUser(@RequestBody Usuario usuario){usuServi.updateUser(usuario);}
 
+    //--------------
 
+//    @GetMapping("/users")
+//    public ResponseEntity<List<User>> getUsers () {
+//        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/users/{id}")
+//    public ResponseEntity<User> getUser (@PathVariable("id") Long id) {
+//        return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
+//    }
+//
+//    @PutMapping("/users/{id}")
+//    public ResponseEntity<User> updateUser (@PathVariable("id") Long id, @RequestBody Map<String, Object> user) {
+//        User newUser = new User(
+//                (String) user.get("username"),
+//                (String) user.get("password"),
+//                (String) user.get("email"),
+//                (String) user.get("name"),
+//                (String) user.get("address"),
+//                (String) user.get("phone")
+//        );
+//
+//        return new ResponseEntity<>(userService.updateUser(id, newUser), HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/users/{id}/cart")
+//    public ResponseEntity<List<CartItem>> getUserCart (@PathVariable("id") Long id) {
+//        System.out.println(userService.getUser(id).getCartItems().size());
+//        return new ResponseEntity<>(userService.getUser(id).getCartItems(), HttpStatus.OK);
+//    }
+//
+//    @PostMapping("/users/{id}/cart/add/{productId}")
+//    public ResponseEntity<User> addToUserCart (@PathVariable("id") Long id,
+//                                               @PathVariable("productId") Long productId) {
+//        User user = userService.getUser(id);
+//        Product product = productService.getProduct(productId);
+//
+//        CartItem cartItem = new CartItem(user, product, 1);
+//        cartItemService.addCartItem(cartItem);
+//
+//        return new ResponseEntity<>(userService.getUser(id), HttpStatus.CREATED);
+//    }
+//
+//    @PutMapping("/users/{id}/cart/update/{productId}")
+//    public ResponseEntity<User> updateCartItem (@PathVariable("id") Long id,
+//                                                @PathVariable("productId") Long productId,
+//                                                @RequestBody CartItem cartItem) {
+//        User user = userService.getUser(id);
+//        Product product = productService.getProduct(productId);
+//
+//        cartItem.setPk(new CartItemPK(user, product));
+//        cartItemService.updateCartItem(cartItem);
+//
+//        return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping("/users/{id}/cart/remove/{productId}")
+//    public ResponseEntity<User> removeFromUserCart (@PathVariable("id") Long id,
+//                                                    @PathVariable("productId") Long productId) {
+//        cartItemService.deleteCartItem(id, productId);
+//
+//        return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
+//
+//    }
 }
 
 
