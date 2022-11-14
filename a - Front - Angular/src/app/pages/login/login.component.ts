9ddea4213c5814446133c1cd62b8070e3130b593
FlorenciaBravoConsultoresIT <florcorvalan26@gmail.com>
2022-11-13 22:26:49 -0300

    Update login.component.ts

--- a - Front - Angular/src/app/pages/login/login.component.ts
+++ a - Front - Angular/src/app/pages/login/login.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 //import { LoginRequest, Usuario, UsuarioService } from 'src/app/services/usuario.service';
 import { NgxRolesService } from 'ngx-permissions';
 import { Token } from 'src/app/models/Token';
+import { User } from 'src/app/models/User';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -18,8 +19,9 @@ export class LoginComponent implements OnInit {
   username : string="";
   password : string="";
   error : string="";
+  usuario: User = new User();
 
-  constructor(private usersService : UsuarioService, private router : Router) { }
+  constructor(private usersService : UsuarioService, private router : Router, private rolesService: NgxRolesService) { }
 
   ngOnInit(): void {
       if (localStorage.getItem('token')) {
@@ -29,10 +31,13 @@ export class LoginComponent implements OnInit {
 
   logIn () {
       this.error = ''
+      this.usuario.username=this.username;
+      this.usuario.password=this.password;
 
       this.usersService.login(
           this.username, this.password).subscribe((token : Token) => {
               localStorage.setItem('token', token.token);
+              this.rolesService.addRole(this.usuario.rol, []);
               this.router.navigateByUrl('/contacto').then(() => window.location.reload())
           }, (error : ErrorEvent) => {
               console.log(error);
